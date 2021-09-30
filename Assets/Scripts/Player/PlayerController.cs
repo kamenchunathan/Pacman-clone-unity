88d2cc172ac4e4b676cde646a05b7847198cb467
Nathan <kamenchunathan@gmail.com>
2021-09-30 18:42:11 +0300

    init: Initialize Pacman Clone repo with a basic working project

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+	[SerializeField]
+	float playerSpeed = 10.0f;
+
+	Animator playerAnimator;
+
+	int currentTravelDirection = 0;
+
+	void Start(){
+		playerAnimator = GetComponent<Animator>();
+	}
+
+	void Update(){
+		float xInput = Input.GetAxis("Horizontal");
+		float yInput = Input.GetAxis("Vertical");
+
+		Vector3 inputVector = new Vector3(xInput, yInput, 0).normalized;
+
+		// Set the appropriate animation according to direction 
+		int newDirection = getMostSignificantDirection(inputVector);
+		if (currentTravelDirection != newDirection){
+			playerAnimator.SetInteger("Animation", newDirection);
+			currentTravelDirection = newDirection;
+		}
+
+		transform.Translate(inputVector * playerSpeed * Time.deltaTime);
+	}
+
+	int getMostSignificantDirection(Vector3 normalizedInputVector){
+		float x = normalizedInputVector.x;
+		float y = normalizedInputVector.y;
+
+		if((int)x == 0 && (int)y==0)
+			return currentTravelDirection;
+
+		return Math.Abs(x) > Math.Abs(y) ? 2 - Convert.ToInt32(x) : 1 + Convert.ToInt32(y);
+	}
+
+}
